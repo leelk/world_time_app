953247487bde8b779581dd138073e7bde3615f87
leelk <karunarathne1995@gmail.com>
2022-01-12 23:39:34 +0530

    Add style to home screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -23,6 +23,7 @@ class _LoadingState extends State<Loading> {
       'location': instance.location,
       'time': instance.time,
       'flag': instance.flag,
+      'dayTime': instance.isDayTime
     });
 
   }
