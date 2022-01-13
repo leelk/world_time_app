289e02df6e448336bda0de2eeeb411f548708855
leelk <karunarathne1995@gmail.com>
2022-01-13 23:00:25 +0530

    Add List View Builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -33,6 +33,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
+      body: ListView.builder(
+        itemCount: locations.length ,
+        itemBuilder: (context,index){
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+            ),
+          );
+        },
+      ),
     );
   }
 }
